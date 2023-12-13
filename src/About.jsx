d91cc5652ae8777f1834724ec60f5877640c8e89
Felix A. Taveras <felixataveras1@gmail.com>
2023-12-12 20:45:15 -0500

    added info in about page

--- src/About.jsx
+++ src/About.jsx
@@ -17,20 +17,20 @@ class Profile extends Component {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Card.Link href="#">GitHub</Card.Link>
-        <Card.Link href="#">LinkedIn</Card.Link>
+        <Card.Link href="https://github.com/ekowyawson">GitHub</Card.Link>
+        <Card.Link href="#">https://www.linkedin.com/in/ekowyawson71/</Card.Link>
       </Card.Body>
     </Card>
-    <Card border="danger" style={{ width: '18rem' }}>
+    <Card border="danger" style={{ width: '18rem', marginBottom: '10px' }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title>Felix A. Taveras</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Front-End</Card.Subtitle>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="https://github.com/f-taveras">GitHub</Card.Link>
+        <Card.Link href="https://www.linkedin.com/in/f-taveras">LinkedIn</Card.Link>
       </Card.Body>
     </Card>
     </>
