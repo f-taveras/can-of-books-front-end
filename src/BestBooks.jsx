454c30fe3708a4255d1ca900ed2bea4ea7c0c7ca
Felix A. Taveras <felixataveras1@gmail.com>
2023-12-13 11:00:11 -0500

    addBooks

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -8,10 +8,51 @@ const URL = import.meta.env.VITE_LOCAL_MONGO || 'default_fallback_value';
 
 const BestBooks = () => {
   const [books, setBooks] = useState([]);
+  const [description,setDescription] = useState('')
+  const [title, setTitle] = useState('')
 
   useEffect(() => {
     fetchBooks();
   }, []);
+
+const handleChange = (e) => {
+  let name = e.target.name;
+  let value = e.target.value;
+
+
+  if (name === "title"){setTitle(value);}
+  if (name === "description"){setDescription(value);}
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  let book = {title, description};
+  console.log("books sent", book)
+
+  let response = await axios.post(`${URL}/addBooks?title=${title}&description=${description}`)
+  console.log("server response", response.data)
+
+  setBooks([...books, response.data])
+}
+
+const handleDelete = async (e) => {
+  console.log("Delete", e.target.id);
+  let response = await axios.delete(`${URL}/books/${e.target.id}`)
+  let deletedBookId = e.target.id;  // Renamed the variable for clarity
+  let newBooks = books.filter((book) => {
+    return book._id !== deletedBookId;  // Updated to use the renamed variable
+  });
+  setBooks(newBooks);
+}
+
+useEffect(() => {
+
+  console.log("done mounting")
+  fetchBooks();
+  return () => {
+    console.log("Peace out! Unmounted")
+  }
+}, []);
 
   const fetchBooks = async () => {
     try {
@@ -26,18 +67,23 @@ const BestBooks = () => {
     <>
       <h2>Best Books</h2>
       {books.length > 0 ? (
-        <Carousel>
+        <Carousel 
+          style={{
+            width: "60%",
+            margin: "auto"
+            }}>
           {books.map((book, idx) => (
-            <Carousel.Item key={idx}>
-              <img
+            <Carousel.Item  key={idx}>
+              <img 
                 className="d-block w-100"
-                src="https://placehold.co/800x400?text=Hello"
+                src={`https://placehold.co/800x400?text=${book.title}`}
                 alt={book.title}
               />
               <Carousel.Caption>
                 <h5>{book.title}</h5>
                 <p>{book.status}</p>
                 <p>{book.description}</p>
+                <span id={book._id} onClick={handleDelete} style={{marginLeft: '.5em', color:'red', cursor:'pointer'}}> Delete book </span>
               </Carousel.Caption>
             </Carousel.Item>
           ))}
@@ -45,6 +91,14 @@ const BestBooks = () => {
       ) : (
         <p>No books available.</p>
       )}
+
+      <hr />
+      <form style={{textAlign:'center'}}onSubmit={handleSubmit}>
+        <input name='title' placeholder='Book title' onChange={handleChange} />
+        <input name='description' placeholder='Type a description' onChange={handleChange} />
+        <button type='submit'>Add Book!</button>
+      </form>
+      <hr />
     </>
   );
 };
